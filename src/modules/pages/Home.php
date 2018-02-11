3349c62d5c59b242882f556ac18aab43945453f0
MINI-ITX\denis <deneus18@hotmail.com>
2018-02-11 18:43:18 +1100

    fixing home page

--- src/modules/pages/Home.php
+++ src/modules/pages/Home.php
@@ -55,20 +55,20 @@ class Home implements ApplicationView
     private function generateTable() {
         global $directory;
 
+        $html = '<div class="mb-4 text-right">
+                    <a href="' . $directory . '/newEntry.html" class="btn btn-primary pl-2"><i class="fa fa-plus mr-1"></i>Add new entry</a>
+                    &nbsp;
+                    <a href="' . $directory . '/accessDelegation.html" class="btn btn-primary pl-2"><i class="fa fa-user-md mr-1"></i>Delegate the access</a>
+                 </div>';
+
         if (count($this->hashes) === 0 || ($this->hashes[0] === NULL)) {
-            $html = '<div class="row border highlight-background pt-3 pb-3 pl-3">
+            $html .= '<div class="row border highlight-background pt-3 pb-3 pl-3">
                 You don\'t have any medical entry at the moment. <br /><br />
                 Either create an entry yourself by using New Entry, either delegate access to your doctor.
             </div>';
             return $html;
         }
 
-        $html = '<div class="mb-4 text-right">
-                    <a href="' . $directory . '/newEntry.html" class="btn btn-primary pl-2"><i class="fa fa-plus mr-1"></i>Add new entry</a>
-                    &nbsp;
-                    <a href="' . $directory . '/accessDelegation.html" class="btn btn-primary pl-2"><i class="fa fa-user-md mr-1"></i>Delegate the access</a>
-                 </div>';
-        
         $html .= '<table id="listOfNotes" class="table table-sm table-hover table-striped">';
         $html .= '<thead class="thead-dark">
             <tr>
