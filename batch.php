362000e48721bcd9611fb0db09f28da62108442a
pugganth <anthony.puggioni@netstarter.com>
2018-02-26 17:43:39 +1100

    Smart contract feature. Signature being implemented

---
+++ batch.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use NeoPHP\NeoWallet;
+use NeoPHP\NeoPHP;
+use HealthChain\modules\classes\Neo;
+
+//$neoPHP = new NeoPHP();
+//$wallet = new NeoWallet('L1JQqSX4M1HS9nY3nMHs3w2DPzbMxcLo2dwXfk584jJx7GqnUMCM');
+$contract = Neo\Contract::getInstance('L1JQqSX4M1HS9nY3nMHs3w2DPzbMxcLo2dwXfk584jJx7GqnUMCM', false);
+$signature = $contract->registerMaster('master');
+print_r($signature);
--- src/modules/classes/Neo/Contract.php
+++ src/modules/classes/Neo/Contract.php
@@ -3,6 +3,9 @@
 namespace HealthChain\modules\classes\Neo;
 
 
+use NeoPHP\Crypto\Hash;
+use Elliptic\EC;
+
 class Contract
 {
     const CONTRACT_HASH = '0x930505c79fd24411e854e93eec08f71b861731fdeb30217dbd0cbc52adc9dbf0';
@@ -10,20 +13,22 @@ class Contract
     const METHOD_DELEGATE = 'delegate';
 
     protected $_neo;
-    protected $_instance;
+    protected static $_instance;
+    protected $_wallet;
 
-    private function __construct()
+    private function __construct($wallet, $mainNet = true)
     {
-        $this->neo = new \NeoPHP\NeoRPC(false);
+        $this->neo = new \NeoPHP\NeoRPC($mainNet);
         $this->neo->setNode($this->neo->getFastestNode());
+        $this->_wallet = $wallet;
     }
 
-    public function getInstance()
+    public static function getInstance($wallet, $mainNet = true)
     {
-        if($this->_instance === null){
-            $this->_instance = new Contract();
+        if(self::$_instance === null){
+            self::$_instance = new Contract($wallet, $mainNet);
         }
-        return $this->_instance;
+        return self::$_instance;
     }
 
     public function getMaster($publicAdr)
@@ -33,7 +38,44 @@ class Contract
 
     public function registerMaster($master)
     {
+
+        $rawTransaction = $this->_generateRawTx(self::METHOD_REGISTER, ['master' => 'masterHash'], $this->_wallet);
+        return $rawTransaction;
+        //$rawTransaction = $this->_ge
+//        $this->neo->sendRawTransaction();
         //TODO: Implement proper trigger to the Smart Contract;
+    }
+
+    protected function _generateRawTx($method, $params, $key)
+    {
+        //TODO: Implement tx generation
+        $tx = '';
+
+        $signature = $this->_generateSignature($tx, $key );
+        return $signature;
+
+//        $signature = $this->_generateSignature($tx, $this->_wallet->getPrivateKey());
+
+        $rawTransaction = '';
+
+        return $rawTransaction;
+    }
+
+    /**
+     * generate a signature of the transaction based on a given private key
+     * @param $tx : Serialized unsigned transaction
+     * @param $privateKey: Private Key
+     * @return $signature: Signature - does not include the tx
+     */
+    protected function _generateSignature($tx, $privateKey)
+    {
+        $ec = new EC('secp256k1');
+        $txHash = bin2hex(Hash::SHA256($tx));
+        var_dump($privateKey);
+        $key = $ec->keyFromPrivate(bin2hex($privateKey));
+        var_dump($key);die();
+        $signature = bin2hex($ec->sign($txHash, $key));
+        return $signature;
     }
 
 }
